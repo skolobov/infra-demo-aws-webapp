5959aa067eb3949681662a82e8051155e29a6f78
Sergei Kolobov <skolobov@gmail.com>
2024-09-02 19:08:53 +0200

    Add AWS Load Balancer Controller (#8)
    * Add Helm provider to Terraform
    * Install AWS Load Balancer Controller via Helm
    * Create required IRSA role

---
+++ terraform/load-balancer.tf
@@ -0,0 +1,55 @@
+module "load_balancer_controller_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.41.0" # checkov:skip=CKV_TF_1
+
+  role_name = "${var.prefix}-load-balancer-controller-role"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    eks = {
+      provider_arn = module.eks.oidc_provider_arn
+
+      namespace_service_accounts = [
+        "kube-system:aws-load-balancer-controller",
+      ]
+    }
+  }
+}
+
+resource "helm_release" "load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.8.2"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "region"
+    value = var.region
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+
+  depends_on = [
+    module.load_balancer_controller_irsa_role,
+  ]
+}
